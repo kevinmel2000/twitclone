175865b8d427df973873f664f1b734cfdc597aa2
tokanggit <tokang.dev@gmail.com>
2020-01-05 11:37:28 +0700

    fix authentication

---
+++ app/Http/Controllers/TimelineController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TimelineController extends Controller
+{
+    public function index() {
+      return view('home');
+    }
+}
